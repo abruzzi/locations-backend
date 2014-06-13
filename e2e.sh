97730070fce51ec870772e8d0ec2f65407bdf983
Qiu Juntao <juntao.qiu@gmail.com>
2014-06-13 23:54:46 +1000

    fix e2e script bug(cannot report the results correctly)

--- e2e.sh
+++ e2e.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# update the dependency
 bundle install
+
+# launch the application
 echo "launch the application"
 rackup > /dev/null 2>&1 &
 PID=$!
+
+# wait for it to start up
 sleep 5
+
+# run the cucumber features and record the status
 cucumber features/
+RES=$?
+
+# terminate after cucumber
 echo "terminate the application"
 kill $PID
+
+# now we know whether the cucumber success or not
+exit $RES
