b875ec57e3a3243fd1a8c7f0a4aaba3b90b01303
Qiu Juntao <juntao.qiu@gmail.com>
2014-06-14 12:29:17 +1000

    add shim step

--- features/step_definitions/search-steps.rb
+++ features/step_definitions/search-steps.rb
@@ -9,6 +9,7 @@ Then /^I should see the search box$/ do
 end
 
 When /^I type "([^"]*)" in search box$/ do |location|
+    @location = location
     fill_in 'locationInput', :with => location
 end
 
@@ -17,6 +18,7 @@ And /^I click search button$/ do
 end
 
 Then /^there are (\d)+ locations show up$/ do |number|
+    page.should have_content(@location)
     all('ul li .title').length.should eq number.to_i
 end
 
