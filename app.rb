77785af0ce05c093b718aebe09ec0f8bde532f3c
Qiu Juntao <juntao.qiu@gmail.com>
2015-07-21 19:15:40 +1000

    add content-type to header

--- app.rb
+++ app.rb
@@ -6,32 +6,31 @@ require 'json'
 
 class Locations < Sinatra::Base
     set :public_folder,  File.dirname(__FILE__)
-    
+
     register Sinatra::CrossOrigin
 
     configure do
         enable :cross_origin
     end
 
-    def initialize 
+    before do
+      content_type "application/json"
+    end
+
+    def load_data
+      @locations = JSON.parse(File.read('locations.json'))
+    end
+
+    def initialize
         super
-        @locations = JSON.parse(File.read('locations.json'))
+        load_data
     end
 
     get '/locations' do
+      if params[:location]
+        @locations.select {|item| item["name"].downcase.include?(params[:location].downcase)}.to_json
+      else
         @locations.to_json
+      end
     end
-
-    get '/locations/:location' do
-        @locations.select {|item| item["name"].include?(params[:location])}.to_json
-    end
-
-    # get '/' do
-    #     File.open('index.html')
-    # end
-
-    # get '/test' do
-    #     File.open('SpecRunner.html')
-    # end
 end
-
