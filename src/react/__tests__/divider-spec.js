782202018aed2bb33b7dc09361798df90e4d493d
Qiu Juntao <juntao.qiu@gmail.com>
2015-07-22 23:33:06 +1000

    fuck that, finally got the first jest test running, node 0.10.38

--- src/react/__tests__/divider-spec.js
+++ src/react/__tests__/divider-spec.js
@@ -1,11 +1,11 @@
 jest.dontMock('../components/divider.jsx');
-console.log('---------------------');
-describe('Divider', function() {
-  it('#render', function() {
-    var React = require('react/addons');
-    var Divider = require('../components/divider.jsx');
-    var TestUtils = React.addons.TestUtils;
 
+describe('Divider', function() {
+  var React = require('react/addons');
+  var Divider = require('../components/divider.jsx');
+  var TestUtils = React.addons.TestUtils;
+
+  it('#render', function() {
     var divider = TestUtils.renderIntoDocument(<Divider />);
     var hr = TestUtils.findRenderedDOMComponentWithTag(divider, 'hr');
     expect(hr.getDOMNode()).toBeDefined();
